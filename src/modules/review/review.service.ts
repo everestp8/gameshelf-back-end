28375dc15ca558e8e56a14675fff2e449f1a50d3
edas1dev <erickdavi200308@gmail.com>
2025-12-07 18:24:07 -0300

    The review service has been modified to add a game to the database if it doesn't already exist during the evaluation process

--- src/modules/review/review.service.ts
+++ src/modules/review/review.service.ts
@@ -9,9 +9,40 @@ export class ReviewService {
     ) {}
 
     async create(review: CreateReviewDTO, authorId: string): Promise<ReadReviewDTO> {
+        await this.ensureGameExists(review.gameId);
         return this.prismaService.review.create({
             data: fromCreateReviewDTO(review, authorId)
         }).then(toReadReviewDTO);
+    }
+
+    private async ensureGameExists(gameId: string) {
+        const existingGame = await this.prismaService.game.findUnique({
+            where: { id: gameId }
+        });
+
+        if (existingGame) return existingGame;
+
+        const RAWG_API_KEY = process.env.RAWG_API_KEY;
+        const response = await fetch(`https://api.rawg.io/api/games/${gameId}?key=${RAWG_API_KEY}`);
+        if (!response.ok) {
+            const text = await response.text();
+            console.error("RAWG Error:", text);
+            throw new Error(`Erro na RAWG API: ${response.status} - ${text}`);
+        }
+
+        const gameData = await response.json();
+
+        if ((gameData as any).detail === "Not found.") {
+            throw new Error(`Game ID ${gameId} não encontrado na API externo RAWG`);
+        }
+
+        return this.prismaService.game.create({
+            data: {
+                id: String(gameData.id),
+                title: gameData.name,
+                gameData: gameData
+            }
+        });
     }
 
     async findByUserId(authorId: string): Promise<ReadReviewDTO[]> {
@@ -26,4 +57,3 @@ export class ReviewService {
         }).then(reviews => reviews.map(toReadReviewDTO));
     }
 }
-
